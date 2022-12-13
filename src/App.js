e8c27918a8faf9db1a035e36d3b8160ca31fa487
João Pedro Caetano <caetanojpo@gmail.com>
2022-12-13 18:26:27 -0300

    Componente Times
    Componente Times implementado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './Components/Banner';
 import Form from './Components/Form';
+import Times from './Components/Times';
 
 function App() {
 
@@ -15,6 +16,14 @@ function App() {
     <div className="App">
       <Banner />
       <Form aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Times nome="Programação"></Times>
+      <Times nome="Front-End"></Times>
+      <Times nome="Data Science"></Times>
+      <Times nome="DevOps"></Times>
+      <Times nome="Mobile"></Times>
+      <Times nome="UX e Design"></Times>
+      <Times nome="Inovação"></Times>
+
     </div>
   );
 }
---
+++ src/Components/Times/index.js
@@ -0,0 +1,11 @@
+import './Times.css'
+
+const Times = (props) => {
+    return (
+        <section className='time'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Times;
